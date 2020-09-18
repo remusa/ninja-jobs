2b42443704e5394951281ff48a5ffa5d8199d7dc
R Sánchez <rene.as.himself@gmail.com>
2020-09-17 19:44:54 -0500

    Custom Layout & Nav

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,21 +4,21 @@
   }
 
   h1 {
-  font-size: 2em;
-  text-transform: uppercase;
-  font-weight :700;
-  margin: 0 0 0.5em 0;
+    font-size: 2em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
   }
 
   img {
-  width: 200px;
-  margin: 0 auto;
-  display: block;
+    width: 200px;
+    margin: 0 auto;
+    display: block;
   }
 
   div {
-  margin-top: 60px;
-  text-align: center;
+    margin-top: 60px;
+    text-align: center;
   }
 
 	@media (min-width: 480px) {
@@ -37,4 +37,4 @@
   <h1>Jobs for Ninjas!</h1>
   <p>Find your next assignment right here.</p>
   <a href="jobs" class="btn">View Jobs</a>
-<div>
+</div>
